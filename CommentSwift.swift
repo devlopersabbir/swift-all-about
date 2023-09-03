0dedb9377d090394e0cef3c60cabf19d29b2b002
devlopersabbir <devlopersabbir@gmail.com>
2023-09-03 12:22:24 +0600

    Comments in Swift

---
+++ CommentSwift.swift
@@ -0,0 +1,9 @@
+// for printing my name
+var myName:String = "Sabbir Hossain"
+/* Showing my existing varibale using print method */
+/*
+*
+* Swift Comment 
+*
+*/
+print(myName) // output: Sabbir Hossain
